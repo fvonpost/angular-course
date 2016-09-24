7fdd350b9963a9780029eb16796a7182daf453ed
Fredrik von Post <fredrik.vonpost@gmail.com>
2016-09-24 21:26:04 +0200

    All functionality in place

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,28 +1,56 @@
 (function () {
   'use strict'
-  angular.module('LunchCheck', [])
-  .controller('LunchCheckController', LunchCheckController);
 
-  LunchCheckController.$inject = ['$scope'];
+  angular.module('ShoppingListCheckOff', [])
+  .controller('ToBuyShoppingController', ToBuyShoppingController)
+  .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-  function LunchCheckController ($scope) {
-    $scope.list = "";
-    $scope.message = "";
+  ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+  AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
 
-    $scope.checkAmount = function () {
-      if ($scope.list.length == 0) {
-        $scope.message = "Please enter data first";
-      }
-      else {
-        if ($scope.list.split(',').length < 4) {
-          $scope.message = "Enjoy!";
-        }
-        else {
-          $scope.message = "Too much!";
-        }
-      }
+  function ToBuyShoppingController (ShoppingListCheckOffService) {
+    var tobuy = this;
+    tobuy.items = ShoppingListCheckOffService.getToBuyItems();
 
+    tobuy.removeItem = function (index) {
+      ShoppingListCheckOffService.removeItem(index);
     };
+
+  }
+
+  function AlreadyBoughtShoppingController (ShoppingListCheckOffService) {
+    var bought = this;
+    bought.items = ShoppingListCheckOffService.getBoughtItems();
+  }
+
+  function ShoppingListCheckOffService () {
+    var service = this;
+
+    // To buy list
+    var toBuyItems = [
+      {name: "cookies", quantity: 10}, // To make Yaakov happy
+      {name: "chips", quantity: 100},  // This is for me
+      {name: "bloke", quantity: 200},  // Have a Bloke, have a smile
+      {name: "root beer", quantity: 12},
+      {name: "cheese doodles", quantity: 10}
+    ];
+
+    // Already bought list
+    var boughtItems = [];
+
+    service.getToBuyItems = function () {
+      return toBuyItems;
+    };
+
+    service.getBoughtItems = function () {
+      return boughtItems;
+    };
+
+    service.removeItem = function (index) {
+      boughtItems.push(toBuyItems.splice(index, 1)[0]);
+    };
+
   }
 
 })();
